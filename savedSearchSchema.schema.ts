a6c709389a7a2b8f3336c8b31e14d4746d13cb38
Berend Vandenbussche <berend@inuits.eu>
2023-07-28 13:48:26 +0200

    Remove duplicate types

--- savedSearchSchema.schema.ts
+++ savedSearchSchema.schema.ts
@@ -17,9 +17,6 @@ export const savedSearchSchema = gql`
   }
 
   enum SearchInputType {
-    AdvancedInputMediaFilesType
-    AdvancedInputType
-    SimpleInputtype
     AdvancedSavedSearchType
   }
 
